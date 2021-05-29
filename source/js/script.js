5861513e27919c275c927d69b72be7f0a0bfeb41
Viseko <viseko23@gmail.com>
2021-05-29 19:46:06 +0700

    Вёрстка мобильной менюшки

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,37 @@
 (function () {
+  // Глобальные состояния
+  let mainMenuOpen = false;
+
+  // Мобильное меню навигации
+
+  // Мобильное меню каталога
+  document.querySelector(".js-menu-nav").addEventListener("click", function() {
+    if (!mainMenuOpen) {
+      openMobileMenu();
+    } else {
+      closeMobileMenu();
+    }
+  });
+
+  function openMobileMenu() {
+    document.body.classList.add("_menu-show");
+    mainMenuOpen = true;
+  }
+
+  function closeMobileMenu() {
+    document.body.classList.remove("_menu-show");
+    mainMenuOpen = false;
+  }
+
+  window.addEventListener("resize", function() {
+    if (window.innerWidth > 875 && mainMenuOpen) closeMobileMenu();
+  });
+
   // Раскрытие подменю каталога
   const catalogExpandBtns = document.querySelectorAll(".nav-catalog__btn-expand");
-  for (expandBtn of catalogExpandBtns) {
+  for (let i = 0; i < catalogExpandBtns.length; i++) {
+    expandBtn = catalogExpandBtns[i];
+
     expandBtn.addEventListener("click", function() {
       const openClass = "nav-catalog__category-item--open";
       const alreadyOpened = document.querySelector(`.${openClass}`);
@@ -14,5 +44,4 @@
       this.parentElement.classList.toggle(openClass);
     });
   }
-
 })();
