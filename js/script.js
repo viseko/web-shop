ac52380bd6658d4adac4bce186c4cfdbea364de3
Viseko <viseko23@gmail.com>
2021-06-05 15:46:46 +0700

    Update 2021-06-05T08:46:41.690Z

--- js/script.js
+++ js/script.js
@@ -1,10 +1,134 @@
 (function () {
-  // Раскрытие подменю каталога
-  const catalogExpandBtns = document.querySelectorAll(".nav-catalog__btn-expand");
-  for (expandBtn of catalogExpandBtns) {
-    expandBtn.addEventListener("click", function() {
-      this.parentElement.classList.toggle("nav-catalog__category-item--open");
-    });
+  // Helpers:
+  // ========== Adaptive move ===========
+// How to use:
+// 1. Add "data-am" attribute to HTML-element you need.
+// 2. Attribute value should be fit the pattern:
+//      <targetElementSelector>/<mediaQuery>
+// .......
+// For example: data-am=".block1 / (min-width: 750px) and (max-width: 1000px)"
+// .......
+// When media query mathes the element will be moved into target block.
+// Otherwise it will be putted into its common position.
+
+const adaptiveMoveElems = document.querySelectorAll("[data-am]");
+const queries = {};
+
+for (let i = 0; i < adaptiveMoveElems.length; i++) {
+  const elem = adaptiveMoveElems[i];
+  const elemData = elem.dataset.am.split("/");
+  const target = elemData[0].trim();
+  const mediaQuery = elemData[1].trim();
+
+  const moveData = {
+    "elem": elem,
+    "parent": elem.parentElement,
+    "target": target,
+    "prev": elem.previousElementSibling
+  };
+
+  if (queries[mediaQuery]) {
+    queries[mediaQuery].push(moveData)
+  } else {
+    queries[mediaQuery] = [moveData];
+  }
+}
+
+for (query in queries) {
+  const mediaQuery = window.matchMedia(query);
+  const queryData = queries[query];
+
+  const matchMedia = function() {
+    if (mediaQuery.matches) {
+      for (let i = 0; i < queryData.length; i++) {
+        const target = document.querySelector(queryData[i].target);
+        target.appendChild(queryData[i].elem);
+      }
+    } else {
+      for (let i = 0; i < queryData.length; i++) {
+        if (queryData[i].prev) {
+          queryData[i].prev.insertAdjacentElement("afterend", queryData[i].elem)
+        } else {
+          queryData[i].parent.appendChild(queryData[i].elem);
+        }
+      }
+    }
+  };
+
+  matchMedia();
+
+  mediaQuery.addEventListener("change", matchMedia);
+}
+;
+  function setMenuButton(options) {
+  const btnClass = options.btn;
+  const stateBodyClass = options.stateBodyClass;
+  const mediaWhenMenuClose = options.mediaWhenMenuClose;
+
+  let menuOpen = false;
+
+  document.querySelector(`.${btnClass}`).addEventListener("click", function() {
+    if (!menuOpen) {
+      openMenu();
+    } else {
+      closeMenu();
+    }
+  });
+
+  const media = window.matchMedia(mediaWhenMenuClose);
+  media.addEventListener("change", function() {
+    if (this.matches && menuOpen) closeMenu();
+  });
+
+  function openMenu() {
+    document.body.classList.add(stateBodyClass);
+    menuOpen = true;
   }
 
+  function closeMenu() {
+    document.body.classList.remove(stateBodyClass);
+    menuOpen = false;
+  }
+
+  return {
+    close: closeMenu,
+    open: openMenu,
+    isOpen: function() {
+      return menuOpen;
+    }
+  };
+}
+;
+
+  // Navigation mobile menu
+  const menuBtn = setMenuButton({
+    btn: "js-menu-nav",
+    stateBodyClass: "_menu-show",
+    mediaWhenMenuClose: "(max-height: 875px)"
+  });
+
+  // Catalog mobile menu
+  const catalogBtn = setMenuButton({
+    btn: "js-menu-catalog",
+    stateBodyClass: "_catalog-show",
+    mediaWhenMenuClose: "(max-height: 875px)"
+  });
+
+  // Catalog submenues
+  const catalogExpandBtns = document.querySelectorAll(".nav-catalog__btn-expand");
+  for (let i = 0; i < catalogExpandBtns.length; i++) {
+    expandBtn = catalogExpandBtns[i];
+
+    expandBtn.addEventListener("click", function() {
+      const openClass = "nav-catalog__category-item--open";
+      const alreadyOpened = document.querySelector(`.${openClass}`);
+
+
+      if (alreadyOpened && alreadyOpened != this.parentElement) {
+        alreadyOpened.classList.remove(openClass);
+      }
+
+      this.parentElement.classList.toggle(openClass);
+    });
+  }
 })();
