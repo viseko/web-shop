68fef2b24cbc7cc84b921c63e2a40a776886bb7b
Viseko <viseko23@gmail.com>
2021-09-16 11:58:28 +0700

    Update 2021-09-16T04:58:21.232Z

--- js/script.js
+++ js/script.js
@@ -143,6 +143,47 @@ const promoSlider = new Swiper('.js-promo-slider', {
   }
 });
 ;
+  // //
+
+const categorySliderBreakpoint = window.matchMedia("(max-width: 450px)");
+
+let categorySlider = null;
+
+categorySliderBreakpoint.addEventListener("change", function() {
+  if (this.matches) {
+    console.log("init");
+  } else {
+    console.log("destroy");
+  }
+});
+
+
+function initCategorySlider() {
+  const categorySlider = new Swiper('.js-promo-slider', {
+    speed: 500,
+    loop: true,
+    autoHeight: true,
+
+    pagination: {
+      el: ".promo__pagination",
+      bulletClass: "slider__bullet",
+      bulletActiveClass: "slider__bullet--active",
+      clickable: true,
+      clickableClass: "slider__pagination--clickable",
+      currentClass: "slider__pagination--current",
+      horizontalClass: "slider__pagination--horizontal",
+      modifierClass: "slider__pagination--"
+    },
+
+    navigation: {
+      prevEl: ".js-promo-prev",
+      nextEl: ".js-promo-next"
+    }
+  });
+}
+
+
+;
 
   // Youtube Videos
   // //
